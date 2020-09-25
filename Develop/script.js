efd17b49990e9c605738471c3b92f5a901f9be9e
Kevin Luo <kvn.luo@gmail.com>
2020-09-25 19:30:45 -0400

    set items to local storage, tweaked color code checker

--- Develop/script.js
+++ Develop/script.js
@@ -27,8 +27,6 @@ $(document).ready(function() {
         $("#sixteen").text(four.format('h'));
         $("#seventeen").text(five.format('h'));
 
-        $("#text").css("background-color","red");
-
         var numLetts = {
             nine: 9,
             ten: 10,
@@ -41,13 +39,20 @@ $(document).ready(function() {
             seventeen: 17
         };
 
+        console.log(moment().diff(moment().hours(17).minutes(0).seconds(0),'hours'));
         $.each($(".hourHook"),function() {
             var numConv = numLetts[($(this).find("span").attr('id'))];
-            var hourInt = moment().hours(numConv).seconds(0);
+            var hourInt = moment().hours(numConv).minutes(0).seconds(0);
             // console.log(numLetts[($(this).find("span").attr('id'))]);
-            if (hourInt.diff(moment(),'seconds') < -3600) {
+            // console.log(moment(),'seconds');
+            // console.log(hourInt);
+
+               if (hourInt.diff(moment(),'seconds') < -3600) {
+            // if (hourInt.diff(moment(),'seconds') < -3600) {
                 $(this).next().css("background-color","#d3d3d3");
-            } else if (hourInt.diff(moment(),'seconds') > 0) {
+
+               } else if (hourInt.diff(moment(),'seconds') > 0) {
+            // } else if (hourInt.diff(moment(),'seconds') > 0) {
                 $(this).next().css("background-color","#77dd77");
             } else {
                 $(this).next().css("background-color","#ff6961");
@@ -59,14 +64,37 @@ $(document).ready(function() {
         // $(".col-md-10").css("background-color",)
     } 
 
+    // saving input to local storage
+    $("button").on("click", function (event) {
+        event.preventDefault();
+        var textItem = $(this).prev().val();
+        localStorage.setItem($(this).parent().find("span").attr('id'),textItem.trim());
+
+    });
+
+    window.onbeforeunload = function() {
+        $.each($(".hour"), function() {
+            // console.log($(this).children("span").attr("id"));
+            // console.log(JSON.parse(localStorage.getItem($(this).children("span").attr("id"))));
+            console.log($(this).next());
+            // var items = (localStorage.getItem($(this).children("span").attr("id")));
+            
+            $(this).next().val((localStorage.getItem($(this).children("span").attr("id"))));
+            // $("#currentDay").append($("<div>").val($this).find("span").attr("id"));
+            // console.log(localStorage.getItem($(".container").find("span").attr("id")));
+
+            // var oldItems = localStorage.getItem($(".container").find("span").attr("id"));
+            // $(".container").find(".col-md-10").val(oldItems);
+        })
+    }
+
     // calling function to initially set time
     updateTime();
     updateColor();
 
     // set interval to continuously call function
     setInterval(updateTime, 1000);
+    setInterval(updateColor, 1000);
 
-
-    var b = moment();
 
 });
